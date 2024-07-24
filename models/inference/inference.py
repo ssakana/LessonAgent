07efe0a5d4f75ba55f3d167711ae9184d0910c95
sakana17 <qjh66617@163.com>
2024-07-24 08:14:24 +0000

    update

--- models/inference/inference.py
+++ models/inference/inference.py
@@ -5,13 +5,10 @@ import json
 import torch
 
 
-model_path = 'openbmb/MiniCPM-2B-sft-bf16'
-
-
 if __name__ == '__main__':
     torch.manual_seed(0)
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.bfloat16, device_map='auto', trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained('openbmb/MiniCPM-2B-sft-bf16')
+    model = AutoModelForCausalLM.from_pretrained('openbmb/MiniCPM-2B-sft-bf16', torch_dtype=torch.bfloat16, device_map='auto', trust_remote_code=True)
     # model = AutoPeftModelForCausalLM.from_pretrained(adapter_path, torch_dtype=torch.bfloat16, device_map='auto', trust_remote_code=True)  # load lora adapter
     samples_path = Path(__file__).parent.parent.parent / 'dataset' / 'en-samples-10.json'
     samples = json.loads(samples_path.read_text(encoding='utf-8'))
